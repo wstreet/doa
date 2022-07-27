015777e4ba1e604b58e47c0572d4de806575ac2a
wstreet <wstreet7@outlook.com>
2022-07-28 01:45:10 +0800

    refactor architecture
    Change-Id: I6e8e2754dec12e3367103f8dcabc4ef9d7fa2ccd

---
+++ bin/doa.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+"use strict";
+require = require("esm")(module);
+
+const program = require("commander");
+const commands = require("../lib/commands");
+
+commands.register(program);
+
+program.parse(process.argv);
